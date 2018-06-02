7ed21b19fd98de1a1fe73929175ebfb6528a564d
orlo123 <orlo123@op.pl>
2018-06-02 17:16:45 +0200

    Add project files.

---
+++ carDElegate/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carDElegate
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public Car() { MaxSpeed = 100; }
+        public Car (string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+
+    }
+}
